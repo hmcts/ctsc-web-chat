27f70aae0d7ce66fba5e0e90c8cd095227def2c9
Luca Del Buono <luca.delbuono@gmail.com>
2019-07-10 13:48:05 +0100

    Update release

--- assets/javascript/hmcts-webchat.js
+++ assets/javascript/hmcts-webchat.js
@@ -3,7 +3,7 @@ function parseText(text) {
 }
 
 function webchat_init(customParams) {
-    const version = '0.1.23';
+    const version = '0.1.24';
     const requiredParams = [
         'uuid',
         'tenant',
